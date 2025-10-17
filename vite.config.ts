1aeefcd86bc000ffd219acd32fa2cae675a40d29
bilalarief <bilalarief7@gmail.com>
2025-10-17 07:41:58 +0700

    add finalytics project

--- vite.config.ts
+++ vite.config.ts
@@ -15,6 +15,8 @@ export default defineConfig({
         main: resolve(__dirname, 'index.html'),
         'works/d2l': resolve(__dirname, 'works/d2l/index.html'),
         'works/safina': resolve(__dirname, 'works/safina/index.html'),
+        'works/finalytics': resolve(__dirname, 'works/finalytics/index.html'),
+        'works/finalytics-landing': resolve(__dirname, 'works/finalytics-landing/index.html'),
         'works/onlinegps': resolve(__dirname, 'works/onlinegps/index.html'),
         'works/printon': resolve(__dirname, 'works/printon/index.html'),
         'works/superalink': resolve(__dirname, 'works/superalink/index.html'),
